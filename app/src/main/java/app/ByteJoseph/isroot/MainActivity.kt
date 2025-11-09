b2d7036d39fccd248c7a95f695ad1aff2d3a0a2e
ByteJoseph <josephnjoseph43@gmail.com>
2025-11-09 13:48:57 +0530

    Add button
    Added button

--- app/src/main/java/app/ByteJoseph/isroot/MainActivity.kt
+++ app/src/main/java/app/ByteJoseph/isroot/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.TextView
 import android.widget.ProgressBar
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.scottyab.rootbeer.RootBeer
 import java.util.Timer
@@ -14,12 +15,14 @@ class MainActivity : AppCompatActivity() {
     private lateinit var textview1: TextView
     private lateinit var timer: Timer
     private lateinit var progbar: ProgressBar
+    private lateinit var button1 : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rootBeer = RootBeer(this)
         textview1 = findViewById(R.id.textview1)
         progbar = findViewById(R.id.progressBar)
+        button1 = findViewById(R.id.button1)
 
         timer = Timer()
         val timertask = object : TimerTask() {
@@ -39,7 +42,13 @@ class MainActivity : AppCompatActivity() {
         onBackPressedDispatcher.addCallback(this){
                 finish()
         }
-        timer.schedule(timertask,6000)
+//      timer.schedule(timertask,6000)
+        button1.setOnClickListener {
+            button1.visibility = View.GONE
+            progbar.visibility = View.VISIBLE
+            textview1.visibility = View.VISIBLE
+            timer.schedule(timertask,3000)
+        }
     }
 
 }
